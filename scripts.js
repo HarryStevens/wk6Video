6b1385e51fdffe4a41e17b3c5e49f2a3ace349a8
HarryStevens <harryjosephstevens@gmail.com>
2014-03-03 17:13:21 -0500

    js documentation

--- scripts.js
+++ scripts.js
@@ -2,12 +2,15 @@
  * @author
  */
 
+//Document ready function calls pageLoaded when everything is loaded
 $(document).ready(pageLoaded);
 
+//pageLoaded function loads the Google viz lib and calls googleLoaded when it is loaded
 function pageLoaded(){
 	google.load("visualization", "1", {packages:["corechart"], callback:googleLoaded});
 }
 
+//googleLoaded function will grab the data
 function googleLoaded(){
 	$.get//put in file here
 }
