bd8839f0fb1cc77751353f8e4311c3dab5bf52ed
HarryStevens <harryjosephstevens@gmail.com>
2014-03-03 17:11:34 -0500

    doc ready and google loaded

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/**
+ * @author
+ */
+
+$(document).ready(pageLoaded);
+
+function pageLoaded(){
+	google.load("visualization", "1", {packages:["corechart"], callback:googleLoaded});
+}
+
+function googleLoaded(){
+	$.get//put in file here
+}
