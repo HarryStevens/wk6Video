6beac4352660312739653a055e7feb57097a8ff2
HarryStevens <harryjosephstevens@gmail.com>
2014-03-03 18:14:22 -0500

    scripts working

--- scripts.js
+++ scripts.js
@@ -6,25 +6,29 @@
 $(document).ready(pageLoaded);
 
 //pageLoaded function loads the Google viz lib and calls googleLoaded when it is loaded
-function pageLoaded(){
-	google.load("visualization", "1", {packages:["corechart"], callback:googleLoaded});
+function pageLoaded() {
+	google.load("visualization", "1", {
+		packages : ["corechart"],
+		callback : googleLoaded
+	});
 }
 
 //googleLoaded function will grab the data from the fusion tables and call the function dataLoaded
-function googleLoaded(){
-	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1NS8sD1CJyw9zjppjdvtHeqO3fJcD-mrFffmK0ukC&key=AIzaSyB-QJux9WIJmey5IJYzPImNzg-xP1gpvU8",dataLoaded,"json";
+function googleLoaded() {
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1NS8sD1CJyw9zjppjdvtHeqO3fJcD-mrFffmK0ukC&key=AIzaSyB-QJux9WIJmey5IJYzPImNzg-xP1gpvU8", dataLoaded, "json");
 }
 
 //this function will do any necessary data conversions and display the data
-function dataLoaded(UNEMP){
-	var dataObj = UNEMP.rows;
-	var data = google.visualization.arrayToDataTable(dataObj);
-
-        var options = {
-          title: 'Civilian Unemployment'
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+function dataLoaded(UNEMP) {
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Date');
+	data.addColumn('number', 'Unemployment');
+	data.addRows(UNEMP.rows);
+	var options = {
+		title: 'Civilian Unemployment',
+		height:500
+	};
+	var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
 }
+
