29a00f72314b76da55e3f059811c37a5d7d588e6
HarryStevens <harryjosephstevens@gmail.com>
2014-03-03 19:34:29 -0500

    format code for readability

--- scripts.js
+++ scripts.js
@@ -20,11 +20,12 @@ function googleLoaded() {
 
 //this function will do any necessary data conversions and display the data
 function dataLoaded(UNEMP) {
-	var bigArray = [];//this is the empty array that I will dump the longer set of arrays into
+	var bigArray = [];
+	//this is the empty array that I will dump the longer set of arrays into
 	var dataRows = UNEMP.rows;
-	
+
 	//Extract date string to convert it to actual date
-	for(var i=0;i<dataRows.length;i++){
+	for (var i = 0; i < dataRows.length; i++) {
 		var currRow = dataRows[i];
 		var dateRow = currRow[0];
 
@@ -34,30 +35,46 @@ function dataLoaded(UNEMP) {
 
 		//This is for the value (unemployment percentage)
 		var valRow = currRow[1];
-		
+
 		//Pushing all those arrays into the big array of arrays
-		var dataArray = [momentRowD,valRow];
+		var dataArray = [momentRowD, valRow];
 		bigArray.push(dataArray);
 	}//end for
-		
+
 	var data = new google.visualization.DataTable();
-	
-	data.addColumn('date', 'Date');//Now the type can be a date instead of a string
+
+	data.addColumn('date', 'Date');
+	//Now the type can be a date instead of a string
 	data.addColumn('number', 'Unemployment');
 	data.addRows(bigArray);
 	var options = {
-		legend:{position:'none'},		
-		curveType: 'function',
-		explorer:{actions:['dragToZoom','rightClickToReset'],axis:'horizontal',maxZoomIn:.01},
-		height:500,
-		chartArea:{height:400,width:1000},
-		hAxis:{title:'Date'},
-		vAxis:{title:'Percent unemployed',ticks:[0,2,4,6,8,10,12],format:'#'},
-		selectionMode:'multiple'
+		legend : {
+			position : 'none'
+		},
+		curveType : 'function',
+		explorer : {
+			actions : ['dragToZoom', 'rightClickToReset'],
+			axis : 'horizontal',
+			maxZoomIn : .01
+		},
+		height : 500,
+		chartArea : {
+			height : 400,
+			width : 1000
+		},
+		hAxis : {
+			title : 'Date'
+		},
+		vAxis : {
+			title : 'Percent unemployed',
+			ticks : [0, 2, 4, 6, 8, 10, 12],
+			format : '#'
+		},
+		selectionMode : 'multiple'
 	};
-		
+
 	var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 	chart.draw(data, options);
 
-	}
-	
+}
+
